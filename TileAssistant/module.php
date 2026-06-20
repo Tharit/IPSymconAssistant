9b6fecaa0a115b650559a2cb76b60baf961658bc
Martin Kleinhans <mail@mkleinhans.de>
2026-06-20 13:19:59 +0200

    bugfixes

--- TileAssistant/module.php
+++ TileAssistant/module.php
@@ -57,9 +57,11 @@ class TileAssistant extends IPSModule
             $idMessageLog = $this->ReadPropertyInteger('log');
             $values = AC_GetLoggedValues($idArchive, $idMessageLog, time() - 15 * 60, time(), 10);
             $res = json_encode(
-                array_map(function($item) {
-                    return $item['Value'];
-                }, $values)
+                array_values(
+                    array_map(function($item) {
+                        return $item['Value'];
+                    }, $values)
+                )
             );    
             $this->UpdateVisualizationValue($res);
         }
