37e7bb31692f42949b1309c011a32613638a372f
Martin Kleinhans <mail@mkleinhans.de>
2026-06-20 13:11:55 +0200

    refactored message log handling

--- TileAssistant/module.php
+++ TileAssistant/module.php
@@ -10,24 +10,59 @@ class TileAssistant extends IPSModule
          parent::Create();
 
          $this->RegisterPropertyInteger('message', 0);
+         $this->RegisterPropertyInteger('log', 0);
          $this->SetVisualizationType(1);
     }
 
     public function ApplyChanges() {
         parent::ApplyChanges();
+
+        foreach ($this->GetMessageList() as $senderID => $messageIDs) {
+            foreach($messageIDs as $messageID) {
+                $this->UnregisterMessage($senderID, $messageID);
+            }
+        }
+
+        $this->RegisterMessage($this->ReadPropertyInteger('log'), VM_UPDATE);
     }
 
     public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+        $objectId = $this->ReadPropertyInteger('log');
+        if ($SenderID !== $objectId) return;
+        $this->UpdateVisualizationValue(GetValue($objectId));
     }
 
     public function RequestAction($Ident, $Value)
     {
-	    if($Ident === 'message' || $Ident === 'load') {
+	    if($Ident === 'message') {
             $script = $this->ReadPropertyInteger('message');
             if($script && @IPS_GetScript($script)) {
-                $res = IPS_RunScriptWaitEx($script, ['action'=>$Ident, 'data'=>$Value]);
+                $res = IPS_RunScriptWaitEx($script, json_decode($Value));
                 $this->UpdateVisualizationValue($res);
             }
+        } else if($Ident === 'load') {
+            $data = json_decode($Value);
+            // $data['session'] and $data['speaker'] can be used to restore the session and user in the script
+
+            // The official Archive Handler / Archive Control GUID in IP-Symcon
+            $archiveGuid = "{43192F0B-135B-4CE7-A0A7-1475603F3060}";
+
+            // Get an array of all instance IDs matching this module GUID
+            $archiveInstances = IPS_GetInstanceListByModuleID($archiveGuid);
+
+            // Grab the first instance ID from the array
+            if (empty($archiveInstances)) {
+                return; // No instance found, handle this case as needed
+            }
+            $idArchive = $archiveInstances[0];
+            $idMessageLog = $this->ReadPropertyInteger('log');
+            $values = AC_GetLoggedValues($idArchive, $idMessageLog, time() - 15 * 60, time(), 10);
+            $res = json_encode(
+                array_map(function($item) {
+                    return $item['Value'];
+                }, $values)
+            );    
+            $this->UpdateVisualizationValue($res);
         }
     }
 
